97eb3fce44db5f547d027206b4dd00a030f240d4
Varun <31084854+Borumer@users.noreply.github.com>
2020-02-17 16:20:37 -0500

    Set groundwork and layout for Flytrap

---
+++ pages/add.php
@@ -0,0 +1,4 @@
+<?php
+	require('header.html');
+	require('brand_header.html');
+?>
---
+++ pages/index.php
@@ -0,0 +1,10 @@
+<?php
+require('header.html');
+require('brand_header.html');
+?>
+<div class = "plus-container">
+	<a id = "add-audio" href = "add">+</a>
+</div>
+</div>
+</body>
+</html>
