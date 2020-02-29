65c85ade114fe00dbef08290a8d154c31fe51601
Varun <31084854+Borumer@users.noreply.github.com>
2020-02-29 15:43:09 -0500

    Finish log in with Borum for Flytrap

--- pages/Login.php
+++ pages/Login.php
@@ -3,15 +3,26 @@ $page_title = "Login - Flytrap";
 $css = "<link href = 'css/login.css' rel = 'stylesheet' type = 'text/css'>";
 require('brand_header.html');
 ?>
-<div style = "grid-row: 2; grid-column: 2 / 3;" class = "form-container">
-	<form>
+<div class = "login account-form-container">
+	<form action = "/" method = "post">
 		<fieldset>
 			<legend>Login</legend>
-			<input type = "text" placeholder = "Email" name = "email" id = "email">
-			<input type = "text" placeholder = "Password" name = "password" id = "password">
+			<input type = "text" required placeholder = "Email" name = "email" id = "loginemail">
+			<input type = "password" required placeholder = "Password" name = "password" id = "loginpassword">
 		</fieldset>
+		<input type = "submit" value = "Login">
+	</form>
+	<button onclick = "connectToBorum('Login')"><img height="20" src = "http://cdn.bforborum.com/images/icon.png" style = "margin-right: 10px">Login with Borum</button>
+</div>
+<div class = "register account-form-container">
+	<form action = "/" method = "post">
+		<fieldset>
+			<legend>Register</legend>
+			<input type = "text" placeholder = "Email" name = "email" id = "registeremail">
+			<input type = "password" placeholder = "Password" name = "password" id = "registerpassword">
+		</fieldset>
+		<input type = "submit" value = "Register">
 	</form>
 	<button onclick = "connectToBorum('Register')"><img height="20" src = "http://cdn.bforborum.com/images/icon.png" style = "margin-right: 10px">Sign up with Borum</button>
-	<button onclick = "connectToBorum('Login')"><img height="20" src = "http://cdn.bforborum.com/images/icon.png" style = "margin-right: 10px">Login with Borum</button>
 </div>
 <script src = "scripts/login.js"></script>
