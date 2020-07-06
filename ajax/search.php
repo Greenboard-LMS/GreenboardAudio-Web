81637fbd3107ded77c156a7f91c7fcdd98846e0a
Varun <31084854+Borumer@users.noreply.github.com>
2020-07-06 11:11:46 -0400

    Update search.php

--- ajax/search.php
+++ ajax/search.php
@@ -1,19 +1,22 @@
 <?php
 session_start();
 include('../../flytrap_connect.inc.php');
+require('../pages/uniqueid.php');
 
 $sq = $_GET['q'];
-$q = "SELECT id, file_name, time_created, user_id FROM audio_files WHERE file_name LIKE '%$sq%' AND user_id = {$_SESSION['id']} AND folder_id IS NULL";
+$q = "SELECT id, file_name, time_created, user_id FROM audio_files WHERE file_name LIKE '%$sq%' AND user_id = {$_SESSION['id']}";
 $r = mysqli_query($dbc, $q);
 $encoded = [];
 while ($row = mysqli_fetch_array($r, MYSQLI_BOTH)) {
-    array_push($encoded, $row);
+  $row['afid'] = @alphaid($row['id'], false, 10);
+  array_push($encoded, $row);
 }
 
-$q = "SELECT id, folder_name, time_created, user_id FROM folders WHERE folder_name LIKE '%$sq%' AND user_id = {$_SESSION['id']} AND parent_id = 0";
+$q = "SELECT id, folder_name, time_created, user_id FROM folders WHERE folder_name LIKE '%$sq%' AND user_id = {$_SESSION['id']}";
 $r = mysqli_query($dbc, $q);
 $encoded2 = [];
 while ($row = mysqli_fetch_array($r, MYSQLI_BOTH)) {
+  $row['afid'] = @alphaid($row['id'], false, 10);
   array_push($encoded2, $row);
 }
 
