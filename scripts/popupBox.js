3f33f94dfe0f160e9447d29ecf74a5eb98ebb75e
Varun <31084854+Borumer@users.noreply.github.com>
2021-04-02 15:43:42 -0400

    Show name of file getting renamed in popup box * Implement/extract to handleAllAudioBoxes() * Implement/extract to handleAllFolderBoxes() * Add "additional" parameter to handleActionBox to show current name of audio file using DOM

--- scripts/popupBox.js
+++ scripts/popupBox.js
@@ -1,67 +1,100 @@
-const showBoxBtnEl = document.querySelector('#new-audio-btn');
+const showBoxBtnEl = document.querySelector("#new-audio-btn");
 
-handleActionBox('share', 'audio');
-handleActionBox('delete', 'audio');
-handleActionBox('rename', 'audio');
-handleActionBox('share', 'folder');
-handleActionBox('delete', 'folder');
-handleActionBox('rename', 'folder');
+handleAllAudioBoxes();
+handleAllFolderBoxes();
 
-function handleActionBox(actionName, mediaType) {
-  const actionBoxEl = document.getElementsByClassName(actionName + '-container')[mediaType == 'audio' ? 0 : 1];
-  const showBoxBtnEls = document.querySelectorAll('button.' + actionName + '-' + mediaType);
-  showBoxBtnEls.forEach((item, i) => {
-    item.onclick = () => {
-      actionBoxEl.style.display = "block";
-      actionBoxEl.id = actionName + "-" + mediaType + "-box-" + item.parentElement.parentElement.id.substring(mediaType == 'audio' ? 5 : 7);
-    };
-  });
+function handleAllAudioBoxes() {
+	handleActionBox("share", "audio");
+	handleActionBox("delete", "audio");
+	handleActionBox("rename", "audio", (actionBoxEl, item) => {
+		actionBoxEl.querySelector('input').value = item.parentElement.parentElement.querySelector('p').textContent;
+	});
 }
 
-const fileBoxEl = document.querySelector('.new-file-box-container');
+function handleAllFolderBoxes() {
+	handleActionBox("share", "folder");
+	handleActionBox("delete", "folder");
+	handleActionBox("rename", "folder");
+}
+
+function handleActionBox(
+	actionName,
+	mediaType,
+	additional = actionBoxEl => {}
+) {
+	const actionBoxEl = document.getElementsByClassName(
+		actionName + "-container"
+	)[mediaType == "audio" ? 0 : 1];
+	const showBoxBtnEls = document.querySelectorAll(
+		"button." + actionName + "-" + mediaType
+	);
+	showBoxBtnEls.forEach((item, i) => {
+		item.onclick = () => {
+			actionBoxEl.style.display = "block";
+			actionBoxEl.id =
+				actionName +
+				"-" +
+				mediaType +
+				"-box-" +
+				item.parentElement.parentElement.id.substring(
+					mediaType == "audio" ? 5 : 7
+				);
+			additional(actionBoxEl, item);
+		};
+	});
+}
+
+const fileBoxEl = document.querySelector(".new-file-box-container");
 function handleNewFileBox() {
+	let isDisabled = false;
+	const isVisible = elem =>
+		!!elem &&
+		!!(
+			elem.offsetWidth ||
+			elem.offsetHeight ||
+			elem.getClientRects().length
+		); // source (2018-03-11): https://github.com/jquery/jquery/blob/master/src/css/hiddenVisibleSelectors.js
 
+	showBoxBtnEl.addEventListener("click", showBox);
 
-  let isDisabled = false;
-  const isVisible = elem => !!elem && !!(elem.offsetWidth || elem.offsetHeight || elem.getClientRects().length); // source (2018-03-11): https://github.com/jquery/jquery/blob/master/src/css/hiddenVisibleSelectors.js
+	function showBox(event) {
+		if (!isDisabled) {
+			event.preventDefault();
+			event.stopPropagation();
+			toggleDisplay("block", 0.5);
+			toggleDisabled(true);
+			document.addEventListener("click", outsideClickListener);
+			setTimeout(animateUploadArrow, 500);
+		}
+	}
 
-  showBoxBtnEl.addEventListener('click', showBox);
+	function outsideClickListener(event) {
+		if (!fileBoxEl.contains(event.target) && isVisible(fileBoxEl)) {
+			// or use: event.target.closest(selector) === null
+			toggleDisplay("none", 1);
+			toggleDisabled(false);
+			document.removeEventListener("click", outsideClickListener);
+			document.getElementById("upload-file-progress-bar").style.display =
+				"none";
+		}
+	}
 
-
-  function showBox(event) {
-    if (!isDisabled) {
-      event.preventDefault();
-      event.stopPropagation();
-      toggleDisplay("block", 0.5);
-      toggleDisabled(true);
-      document.addEventListener('click', outsideClickListener);
-      setTimeout(animateUploadArrow, 500);
-    }
-  }
-
-  function outsideClickListener(event) {
-    if (!fileBoxEl.contains(event.target) && isVisible(fileBoxEl)) { // or use: event.target.closest(selector) === null
-      toggleDisplay("none", 1);
-      toggleDisabled(false);
-      document.removeEventListener('click', outsideClickListener);
-      document.getElementById('upload-file-progress-bar').style.display = "none";
-    }
-  }
-
-  function animateUploadArrow() {
-    const imgEl = document.getElementById('upload-file-arrow');
-    imgEl.className = "bounce";
-  }
+	function animateUploadArrow() {
+		const imgEl = document.getElementById("upload-file-arrow");
+		imgEl.className = "bounce";
+	}
 }
 
 function toggleDisplay(display, opacity) {
-    document.querySelectorAll('.grid > div:not(.new-file-box-container)').forEach((item, i) => {
-      item.style.opacity = opacity;
-    });
-    fileBoxEl.style.display = display;
+	document
+		.querySelectorAll(".grid > div:not(.new-file-box-container)")
+		.forEach((item, i) => {
+			item.style.opacity = opacity;
+		});
+	fileBoxEl.style.display = display;
 }
 
 function toggleDisabled(bool) {
-    showBoxBtnEl.attributes.disabled = bool;
-    isDisabled = bool;
+	showBoxBtnEl.attributes.disabled = bool;
+	isDisabled = bool;
 }
