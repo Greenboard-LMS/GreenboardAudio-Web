981b62b658c00e6a000bfcf75dc9be944f009e7f
Varun <31084854+varunsingh87@users.noreply.github.com>
2021-06-23 20:28:07 -0400

    Make audio moving functional again

--- scripts/client/FlytrapRequest.js
+++ scripts/client/FlytrapRequest.js
@@ -1,7 +1,10 @@
 class FlytrapRequest extends BorumRequest {
     static initialize(input, init) {
-        const host = `https://api.audio.borumtech.com`;
-        // const host = `http://localhost:8100`;
+        const host = 
+            window.location.hostname === 'localhost' ? 
+            'http://localhost:8100': 
+            `https://api.audio.borumtech.com`;
+            
         const url = `${host}/v1/${input}`;
         return new FlytrapRequest(url, init);
     }
